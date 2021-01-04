3aca23ec32f9c549543fefd66a8715f1206992ff
mperkonigg <matthias.perkonigg@entenbaer.at>
2021-01-04 11:28:58 +0100

    lung nodules added, cardiac loader updated

--- catinous/dynamicmemory/DynamicMemoryModel.py
+++ catinous/dynamicmemory/DynamicMemoryModel.py
@@ -13,6 +13,7 @@ import torch.nn.functional as F
 import torchvision.models as models
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader
+import numpy as np
 
 from catinous.dataset.ContinuousDataset import *
 from catinous.dataset.BatchDataset import *
@@ -105,6 +106,9 @@ class DynamicMemoryModel(pl.LightningModule):
     def forward(self, x):
         return self.model(x)
 
+    def forward_lidc(self, x, y):
+        return self.model(x, y)
+
     def training_step(self, batch, batch_idx):
         x, y, scanner, filepath = batch
         self.grammatrices = []
@@ -142,14 +146,27 @@ class DynamicMemoryModel(pl.LightningModule):
             self.unfreeze()
 
             x, y = self.trainingsmemory.get_training_batch(self.hparams.batchsize, self.hparams.random_memory)
-            x = x.to(self.device)
-            y = y.to(self.device)
 
-            y_hat = self.forward(x.float())
-            loss = self.loss(y_hat, y.float())
+            if self.hparams.task=='lidc':
+                x = list(i.to(self.device) for i in x)
+                targets = [{k: v.to(self.device) for k, v in t.items()} for t in y]
+                loss_dict = self.forward_lidc(x, targets)
+                loss = sum(l for l in loss_dict.values())
+            else:
+                x = x.to(self.device)
+                y = y.to(self.device)
+
+                y_hat = self.forward(x.float())
+                loss = self.loss(y_hat, y.float())
         else:
-            y_hat = self.forward(x.float())
-            loss = self.loss(y_hat['out'], y)
+            if self.hparams.task == 'lidc':
+                x = list(i.to(self.device) for i in x)
+                targets = [{k: v.to(self.device) for k, v in t.items()} for t in y]
+                loss_dict = self.forward_lidc(x, targets)
+                loss = sum(l for l in loss_dict.values())
+            else:
+                y_hat = self.forward(x.float())
+                loss = self.loss(y_hat['out'], y)
 
         self.grammatrices = []
         self.log('train_loss', loss)
@@ -166,36 +183,39 @@ class DynamicMemoryModel(pl.LightningModule):
         if self.hparams.continuous:
             if self.hparams.task == 'brainage':
                 return DataLoader(BrainAgeContinuous(self.hparams.datasetfile,
-                                                                   transition_phase_after=self.hparams.transition_phase_after),
-                                  batch_size=self.hparams.batch_size, num_workers=2, drop_last=True)
+                                                                   transition_phase_after=self.hparams.transition_phase_after,
+                                                     seed=self.hparams.seed),
+                                  batch_size=self.hparams.batch_size, num_workers=8, drop_last=True)
             elif self.hparams.task == 'lidc':
                 return DataLoader(LIDCContinuous(self.hparams.datasetfile,
-                                                     transition_phase_after=self.hparams.transition_phase_after),
-                                  batch_size=self.hparams.batch_size, num_workers=2, drop_last=True)
+                                                     transition_phase_after=self.hparams.transition_phase_after,
+                                                 seed=self.hparams.seed),
+                                  batch_size=self.hparams.batch_size, num_workers=8, collate_fn=utils.collate_fn)
             elif self.hparams.task == 'cardiac':
                 return DataLoader(CardiacContinuous(self.hparams.datasetfile,
-                                                 transition_phase_after=self.hparams.transition_phase_after),
-                                  batch_size=self.hparams.batch_size, num_workers=2, drop_last=True)
+                                                 transition_phase_after=self.hparams.transition_phase_after,
+                                                    seed=self.hparams.seed),
+                                  batch_size=self.hparams.batch_size, num_workers=8, drop_last=True)
         else:
             if self.hparams.task == 'brainage':
                 return DataLoader(BrainAgeBatch(self.hparams.datasetfile,
                                                   iterations=self.hparams.noncontinuous_steps,
                                                   batch_size=self.hparams.batch_size,
                                                   split=self.hparams.noncontinuous_train_splits),
-                                  batch_size=self.hparams.batch_size, num_workers=2)
+                                  batch_size=self.hparams.batch_size, num_workers=8)
             elif self.hparams.task == 'lidc':
                 return DataLoader(LIDCBatch(self.hparams.datasetfile,
                                                   iterations=self.hparams.noncontinuous_steps,
                                                   batch_size=self.hparams.batch_size,
                                                   split=self.hparams.noncontinuous_train_splits),
-                                  batch_size=self.hparams.batch_size, num_workers=2)
+                                  batch_size=self.hparams.batch_size, num_workers=8, collate_fn=utils.collate_fn)
             elif self.hparams.task == 'cardiac':
                 return DataLoader(CardiacBatch(self.hparams.datasetfile,
                                                   iterations=self.hparams.noncontinuous_steps,
                                                   batch_size=self.hparams.batch_size,
                                                   split=self.hparams.noncontinuous_train_splits,
                                                   res=self.hparams.scanner),
-                                  batch_size=self.hparams.batch_size, num_workers=2)
+                                  batch_size=self.hparams.batch_size, num_workers=8)
 
     #@pl.data_loader
     def val_dataloader(self):
@@ -208,7 +228,8 @@ class DynamicMemoryModel(pl.LightningModule):
             return DataLoader(LIDCBatch(self.hparams.datasetfile,
                                           split='val'),
                           batch_size=4,
-                          num_workers=1)
+                          num_workers=2,
+                          collate_fn=utils.collate_fn)
         elif self.hparams.task == 'cardiac':
             return DataLoader(CardiacBatch(self.hparams.datasetfile,
                                           split=['val']),
@@ -236,6 +257,25 @@ class DynamicMemoryModel(pl.LightningModule):
                 dice_2.append(mut.dice(y[i], y_hat_flat[i], classi=2))
                 dice_3.append(mut.dice(y[i], y_hat_flat[i], classi=3))
             return {'scanner': scanners, 'dice_1': dice_1, 'dice_2': dice_2, 'dice_3': dice_3}
+        elif self.hparams.task == 'lidc':
+            images, targets, scanner, filepath = batch
+            images = list(image.to(self.device) for image in images[0])
+
+            out = self.model(images)
+
+            out_boxes = [
+                utils.filter_boxes_area(out[i]['boxes'].cpu().detach().numpy(), out[i]['scores'].cpu().detach().numpy())
+                for i in range(len(out))]
+            boxes_np = [b[0] for b in out_boxes]
+            scores_np = [b[1] for b in out_boxes]
+
+            final_boxes, final_scores = utils.correct_boxes(boxes_np, scores_np)
+
+            gt = []
+            for t in targets:
+                gt.append(t['boxes'][0])
+
+            return {'final_boxes': final_boxes, 'final_scores': final_scores, 'gt': gt, 'scanner': scanner}
 
     def validation_epoch_end(self, validation_step_outputs):
         if self.hparams.task == 'cardiac':
@@ -256,6 +296,86 @@ class DynamicMemoryModel(pl.LightningModule):
                 self.log(f'val_dice1_{s}', df_mean['dice_1'][s])
                 self.log(f'val_dice2_{s}', df_mean['dice_2'][s])
                 self.log(f'val_dice3_{s}', df_mean['dice_3'][s])
+        elif self.hparams.task == 'lidc':
+            iou_thres = 0.5
+
+            overall_true_pos = dict()
+            overall_false_pos = dict()
+            overall_false_neg = dict()
+            overall_boxes_count = dict()
+            recalls = dict()
+            precision = dict()
+
+            for scanner in self.hparams.order:
+                overall_true_pos[scanner] = dict()
+                overall_false_pos[scanner] = dict()
+                overall_false_neg[scanner] = dict()
+                overall_boxes_count[scanner] = dict()
+                recalls[scanner] = []
+                precision[scanner] = []
+                for k in np.arange(0.0, 1.01, 0.05):
+                    overall_true_pos[scanner][k] = 0
+                    overall_false_pos[scanner][k] = 0
+                    overall_false_neg[scanner][k] = 0
+                    overall_boxes_count[scanner][k] = 0
+
+            for out in validation_step_outputs:
+                final_boxes = out['final_boxes']
+                final_scores = out['final_scores']
+                gt = out['gt']
+                scanner = out['scanner']
+
+                for j, fb in enumerate(final_boxes):
+                    s = scanner[j]
+                    g = gt[j]
+                    fs = final_scores[j]
+
+                    for k in np.arange(0.0, 1.01, 0.05):
+                        false_positives = 0
+                        false_negatives = 0
+                        true_positives = 0
+                        detected = False
+                        boxes_count = 0
+                        if len(fb) > 0:
+                            for i, b in enumerate(fb):
+                                if fs[i] > k:
+                                    boxes_count += 1
+                                    if utils.bb_intersection_over_union(g, b) > iou_thres:
+                                        detected = True
+                                    else:
+                                        false_positives += 1
+                            if detected:
+                                true_positives += 1
+                            else:
+                                false_negatives += 1
+                        overall_true_pos[s][k] += true_positives
+                        overall_false_pos[s][k] += false_positives
+                        overall_false_neg[s][k] += false_negatives
+                        overall_boxes_count[s][k] += boxes_count
+            aps = dict()
+            for scanner in self.hparams.order:
+                for k in np.arange(0.0, 1.01, 0.05):
+                    if (overall_false_neg[scanner][k] + overall_true_pos[scanner][k]) == 0:
+                        recalls[scanner].append(0.0)
+                    else:
+                        recalls[scanner].append(overall_true_pos[scanner][k] / (overall_false_neg[scanner][k] + overall_true_pos[scanner][k]))
+                    if (overall_false_pos[scanner][k] + overall_true_pos[scanner][k]) == 0:
+                        precision[scanner].append(0.0)
+                    else:
+                        precision[scanner].append(overall_true_pos[scanner][k] / (overall_false_pos[scanner][k] + overall_true_pos[scanner][k]))
+
+                prec = np.array(precision[scanner])
+                rec = np.array(recalls[scanner])
+                ap = []
+                for t in np.arange(0.0, 1.01, 0.1):
+                    prec_arr = prec[rec > t]
+                    if len(prec_arr) == 0:
+                        ap.append(0.0)
+                    else:
+                        ap.append(prec_arr.max())
+                aps[scanner] = np.array(ap).mean()
+
+                self.log(f'val_ap_{scanner}', aps[scanner])
 
 def trained_model(hparams, show_progress = False):
     df_cache = None
